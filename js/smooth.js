17d54372fac47dfb798f41157f6756f37f369c3e
TrickkyRicky <60045899+TrickkyRicky@users.noreply.github.com>
2020-07-15 13:15:45 -0500

    Add files via upload

---
+++ js/smooth.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+	let scrollLink = $(".scroll");
+
+	// Smooth scrolling
+	scrollLink.click(function (e) {
+		e.preventDefault();
+		$("body,html").animate(
+			{
+				scrollTop: $(this.hash).offset().top,
+			},
+			1000
+		);
+	});
+
+	// Active link switching
+	$(window).scroll(function () {
+		let scrollbarLocation = $(this).scrollTop();
+
+		scrollLink.each(function () {
+			let sectionOffset = $(this.hash).offset().top - 20;
+
+			if (sectionOffset <= scrollbarLocation) {
+				$(this).addClass("active");
+				$(this).siblings().removeClass("active");
+			}
+		});
+	});
+});
